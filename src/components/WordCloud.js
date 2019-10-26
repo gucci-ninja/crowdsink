4e1d46626bfe41a525e6bcfdfd2b042d876bac62
gucci-ninja <beautyspell5@gmail.com>
2019-10-26 00:37:27 -0400

    words only show as 90 or 0 degrees

--- src/components/WordCloud.js
+++ src/components/WordCloud.js
@@ -3,7 +3,7 @@ import WordCloudd from "react-d3-cloud";
 
 
 const data = [
-  { text: "Hey", value: 1000 },
+  { text: "Hey", value: 1000},
   { text: "lol", value: 200 },
   { text: "first impression", value: 800 },
   { text: "very cool", value: 1000000 },
@@ -11,7 +11,7 @@ const data = [
 ];
 
 const fontSizeMapper = word => Math.log2(word.value) * 5;
-const rotate = word => word.value % 360;
+const rotate = () => Math.random() > 0.5 ? 90 : 0;
 
 class WordCloud extends React.Component {
     render(){

96f468b35710b262f37fcff61fb8eaf76a716b22
kawaiicheese <josephlu116@gmail.com>
2019-10-26 07:28:19 -0400

    small fix to webCrawler; awaiting firebase support

--- src/WebCrawler.js
+++ src/WebCrawler.js
@@ -6,24 +6,25 @@ var rents = "article.list-item";
 var childs = "div.text_content";
 
 
-async function crawl(URL, parent, child) {
+function crawl(URL, parent, child) {
     var texts = [];
-    await request(URL, function (err, res, body) {
+    request(URL, function (err, res, body) {
         if (err) {
             console.log("an error occured : " + err);
         }
         else {
-            let $ = cheerio.load(body);  //loading content of HTML body
+            let $ = cheerio.load(body); //loading content of HTML body
             $(parent).each(function (index) {
                 var text = $(this).find(child).text();
+                
+                // call sentiment function?
+
+
+                // replace with writing to database.
+
                 texts.push(text);
             });
         }
     });
     return texts;
 }
-
-crawl(url, rents, childs).then(function(results){
-    console.log(results);
-})
-

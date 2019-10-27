73163a0ed53d0c7175a7736e53ddc84b9199a9b8
gucci-ninja <beautyspell5@gmail.com>
2019-10-27 00:44:38 -0400

    added a try catch lmao bois

--- src/components/WordCloud.js
+++ src/components/WordCloud.js
@@ -48,22 +48,22 @@ class WordCloud extends React.Component {
 
   // add array [[,],[,],[,],[,],..] of reviews to a company collection [{,},{,},{,},..]
   addData = async (company, arr) => {
-    for (var review of arr) {
-      db.collection('companies')
-        .doc(company)
-        .collection('reviews')
-        .add(
-          {
-            sentiment: review[0],
-            text: review[1],
-            emotion: review[2],
-            keywords: review[3]
-          }
-        )
-    }
+    // const reviews = arr.map((obj)=> {return Object.assign({}, obj)});
+    try {
+      for (var review of arr) {
+        const r = {
+          sentiment: review[0],
+          text: review[1],
+          emotion: review[2],
+          keywords: review[3]
+        }
+        db.collection('companies').doc(company).collection('reviews').add(r);
+      }
+    }catch(e) {
+      console.log('fuck'); 
+   }
     this.getData('JetBlue');
   }
-
   componentDidMount() {
     this.addData('JetBlue', [3.2, 'holy shit it lit', {'happiness':1.2,'sadness':0.3}, 'keyword,1,2,3'])
     this.getData('JetBlue');
